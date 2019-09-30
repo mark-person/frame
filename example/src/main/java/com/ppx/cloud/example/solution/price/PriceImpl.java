66a18511e96c04cdd4cbf846442282a39fae30f2
dengxz <6882855@qq.com>
2019-09-30 14:38:47 +0800

    t

--- example/src/main/java/com/ppx/cloud/example/solution/price/PriceImpl.java
+++ example/src/main/java/com/ppx/cloud/example/solution/price/PriceImpl.java
@@ -18,9 +18,9 @@ public class PriceImpl extends MyDaoSupport {
 	
 	public List<Map<String, Object>> calc() {
 		int num = 4;
-		String countSql = "select sum(sku_price * real_num) total_price from ("
-				+ " select e_cart.*, @r := (@minus := sku_num + @minus) - @minus_num n, if(@r <= 0, 0, if(sku_num - @r <= 0, sku_num, @r)) real_num from e_free_cart, "
-				+ " (select @minus := 0) t0, (select @minus_num := sum(sku_num) div 4 from e_free_cart) t1 order by sku_price) t";
+//		String countSql = "select sum(sku_price * real_num) total_price from ("
+//				+ " select e_cart.*, @r := (@minus := sku_num + @minus) - @minus_num n, if(@r <= 0, 0, if(sku_num - @r <= 0, sku_num, @r)) real_num from e_free_cart, "
+//				+ " (select @minus := 0) t0, (select @minus_num := sum(sku_num) div 4 from e_free_cart) t1 order by sku_price) t";
 		
 		String subSql = "select e_free_cart.*, @r := (@minus := sku_num + @minus) - @minus_num n, if(@r <= 0, 0, if(sku_num - @r <= 0, sku_num, @r)) real_num from e_free_cart, "
 				+ " (select @minus := 0) t0, (select @minus_num := sum(sku_num) div ? from e_free_cart) t1 order by sku_price";
