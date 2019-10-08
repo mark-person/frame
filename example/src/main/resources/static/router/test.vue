f296be5977384866e4ddc8cb3fdb613c762ee212
dengxz <6882855@qq.com>
2019-10-08 19:13:03 +0800

    t

---
+++ example/src/main/resources/static/router/test.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+<div>
+
+new Test()
+
+
+
+
+
+</div>
+
+
+
+<div id="modal" v-cloak>
+<modal v-if="modal.showModal" :modal="modal">
+test001
+</modal>
+</div>
+
+
+</template>
+
+
+
+<script>
+var modal = base.modal("modal");
+// alert(modal);
+modal.show('新增',  {});
+
+// base.showLoading();
+// alert(1);
+
+</script>
+
+
+
