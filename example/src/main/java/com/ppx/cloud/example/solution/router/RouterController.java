eb6cbe60a7729bc99f99c77ece80bc277af9afef
dengxz <6882855@qq.com>
2019-10-08 14:12:43 +0800

    t

---
+++ example/src/main/java/com/ppx/cloud/example/solution/router/RouterController.java
@@ -0,0 +1,24 @@
+package com.ppx.cloud.example.solution.router;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("base/router")
+public class RouterController {
+	
+	@RequestMapping("/router")
+	public ModelAndView router(ModelAndView mv) {
+		
+		return mv;
+	}
+	
+	
+
+	
+
+	
+	
+}
