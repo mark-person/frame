9e676b1f25bdd03b5c646ca879560326dc38055f
dengxz <6882855@qq.com>
2019-10-16 19:51:20 +0800

    t

---
+++ example/src/main/resources/static/router/view.vue
@@ -0,0 +1,40 @@
+
+
+
+<template>
+
+<div>
+
+<a @click="back()" href="javascript:this">back</a>
+
+view001
+
+</div>
+
+
+
+
+</template>
+
+<script>
+
+window.onscroll = function() {}
+
+module.exports =  {
+    name: "app",
+    data:function(){
+        return {
+            name:'tom'
+        }
+    },
+    methods:{
+        back:function() {
+        	goBack();
+        }
+    }
+}
+
+</script>
+
+
+
