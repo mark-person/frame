b1342dafa497b4c4616a28eab55cb3f93ae710bb
dengxz <6882855@qq.com>
2019-10-16 12:50:22 +0800

    t

--- base/src/main/resources/static/mobile/mobile.js
+++ base/src/main/resources/static/mobile/mobile.js
@@ -33,3 +33,39 @@ axios.interceptors.response.use(function(res) {
 })
 
 var base = {};
+
+
+
+
+base.pageScroll = function(e, uri) {
+	var e = e || window.event;
+	var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+	
+	if (scrolltop + document.body.clientHeight >= window.innerHeight - 50) {
+		if (!thisSelf.page.hasMore) return;
+		
+		thisSelf.page.hasMore = false;
+		thisSelf.page.currentNumber++;
+		
+		thisSelf.page.more = false;
+		thisSelf.page.end = false;
+		thisSelf.page.loading = true;
+		
+		axios.post(uri, {pageNumber:thisSelf.page.currentNumber}).then(function(res) {
+			thisSelf.page.loading = false;
+			
+			for (var i = 0; i < res.list.length; i++) {
+				thisSelf.list.push(res.list[i]);
+			}
+			
+			if (res.page.pageSize * res.page.pageNumber < res.page.totalRows) {
+				thisSelf.page.hasMore = true;
+				thisSelf.page.more = true;
+			}
+			else {
+				thisSelf.page.end = true;
+			}
+	    })
+	}
+}
+
--- example/src/main/resources/static/router/home.vue
+++ example/src/main/resources/static/router/home.vue
@@ -10,56 +10,28 @@
 	</div>
 </div>
 
-<div id="pageMsg" v-cloak>
-	<div v-if="noData">0 row</div>
+<div v-cloak>
+	<div v-if="page.noData">0 row</div>
 
-	<div v-if="pageMore"><p>-- 上拉加载更多 --</p></div>
-	<div v-if="pageLoading"><p>-- 加载中... --</p></div>
-	<div v-if="pageEnd"><p>-- 暂时就这么多了 --</p></div>
+	<div v-if="page.more"><p>-- 上拉加载更多 --</p></div>
+	<div v-if="page.loading"><p>-- 加载中... --</p></div>
+	<div v-if="page.end"><p>-- 暂时就这么多了 --</p></div>
 </div>
 
 </template>
 
 <script>
 
-var pageHasMore = true;
-var currentPageNumber = 1;
+
+alert(1);
 window.onscroll = function(e) {
-	var e = e || window.event;
-	var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
-	
-	if (scrolltop + document.body.clientHeight >= window.innerHeight - 50) {
-		if (!pageHasMore) return;
-		
-		pageHasMore = false;
-		currentPageNumber++;
-		
-		thisSelf.pageMore = false;
-		thisSelf.pageEnd = false;
-		thisSelf.pageLoading = true;
-		
-		axios.post("base/router/test", {pageNumber:currentPageNumber}).then(function(res) {
-			thisSelf.pageLoading = false;
-			
-			for (var i = 0; i < res.list.length; i++) {
-				thisSelf.list.push(res.list[i]);
-			}
-			
-			if (res.page.pageSize * res.page.pageNumber < res.page.totalRows) {
-				pageHasMore = true;
-				thisSelf.pageMore = true;
-			}
-			else {
-				thisSelf.pageEnd = true;
-			}
-	    })
-	}
+	base.pageScroll(e, "base/router/test");
 }
 
 module.exports =  {
  	name: "app",
 	data:function() {
-        return {list:[], pageMore:false, pageLoading:false, pageEnd:false, noData:false}
+        return {list:[], page:{more:false, loading:false, end:false, noData:false, hasMore:true, currentNumber:1}}
     },
     methods:{
         xxx:function() {
@@ -69,7 +41,7 @@ module.exports =  {
     	thisSelf = this;
     	axios.post("base/router/test", {pageNumber:1}).then(function(res) {
 			thisSelf.list = res.list;
-			thisSelf.noData = (res.list.length == 0);
+			thisSelf.page.noData = (res.list.length == 0);
 		});
     }
 }
