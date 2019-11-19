01e6ea874c3444d9f8a9e8365371963e6ec9e397
dengxz <6882855@qq.com>
2019-11-19 19:35:50 +0800

    t

---
+++ example/src/main/java/com/ppx/cloud/example/weui/WeuiController.java
@@ -0,0 +1,19 @@
+package com.ppx.cloud.example.weui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class WeuiController {
+	
+	
+	public ModelAndView weui(ModelAndView mv) {
+		
+		
+		return mv;
+	}
+	
+	
+}
